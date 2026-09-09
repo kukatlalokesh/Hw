ac405876ec09ff48387ee2fe3f79269e4ff6c441
kukatlalokesh <lokeshprasad943@gmail.com>
2026-09-09 17:45:17 -0400

    Add Week 1 Day 2 list operations

---
+++ Week1/Day2_ListOperations/list_operations.py
@@ -0,0 +1,18 @@
+numbers = [5, 2, 8]
+
+print("Original list:", numbers)
+
+numbers.append(10)
+print("After append:", numbers)
+
+numbers.insert(1, 7)
+print("After insert:", numbers)
+
+numbers.remove(2)
+print("After remove:", numbers)
+
+numbers.sort()
+print("After sort:", numbers)
+
+numbers.reverse()
+print("After reverse:", numbers)
