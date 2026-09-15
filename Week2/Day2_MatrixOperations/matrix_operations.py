d76dc48cc90aedcdcb2507cdd25af54fd8de103d
kukatlalokesh <lokeshprasad943@gmail.com>
2026-09-15 19:05:25 -0400

    Add Week 2 Day 2 matrix operations exercise

---
+++ Week2/Day2_MatrixOperations/matrix_operations.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+matrix_a = np.array([[1, 2], [3, 4]])
+matrix_b = np.array([[5, 6], [7, 8]])
+
+addition = matrix_a + matrix_b
+multiplication = matrix_a @ matrix_b
+
+print("Matrix A:")
+print(matrix_a)
+
+print("\nMatrix B:")
+print(matrix_b)
+
+print("\nMatrix addition:")
+print(addition)
+
+print("\nMatrix multiplication:")
+print(multiplication)
