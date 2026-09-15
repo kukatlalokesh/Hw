282b9a12841d61097cf9162ff1e1e34b935daa71
kukatlalokesh <lokeshprasad943@gmail.com>
2026-09-15 08:15:34 -0400

    Add NumPy array math exercise

---
+++ Week2/Day1_ArrayMath/array_math.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+first_array = np.array([10, 20, 30, 40])
+second_array = np.array([2, 4, 5, 8])
+
+print("First Array:", first_array)
+print("Second Array:", second_array)
+
+print("\nAddition:")
+print(first_array + second_array)
+
+print("\nSubtraction:")
+print(first_array - second_array)
+
+print("\nMultiplication:")
+print(first_array * second_array)
+
+print("\nDivision:")
+print(first_array / second_array)
